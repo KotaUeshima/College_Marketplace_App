9bc603a3ea7fa07e7e7d68c501c0213e96fdede2
Kota Ueshima <ueshima@usc.edu>
2023-04-04 09:09:30 -0700

    Initial Commit

---
+++ CollegeMarketplace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollegeMarketplace
+//
+//  Created by Kota Ueshima on 4/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
